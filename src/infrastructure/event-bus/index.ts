8090aafd7e3c0c9cf4fd1e337f5b4d5d880cd295
Sang Le <thanhsang6174@gmail.com>
2026-08-04 09:00:00 +0700

    feat(event-bus): add barrel export index

---
+++ src/infrastructure/event-bus/index.ts
@@ -0,0 +1,5 @@
+export * from './event-bus.interface';
+export * from './event-bus.constants';
+export * from './event-bus.decorator';
+export * from './nest-event-bus.service';
+export * from './event-bus.module';
